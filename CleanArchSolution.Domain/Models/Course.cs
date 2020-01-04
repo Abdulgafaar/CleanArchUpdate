0ea1b01a01ed643f751085179de6030782c109c4
ABDULGAFAAR ABIOYE <abdulgafaar.abioye@gmail.com>
2020-01-05 00:05:30 +0100

    Add Course Domain Model

---
+++ CleanArchSolution.Domain/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchSolution.Domain.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
